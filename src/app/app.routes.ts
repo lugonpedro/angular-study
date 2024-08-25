877189b2afbb3abe46c80cc4c31e04d657e4016a
Pedro Lugon <pedromotta20@gmail.com>
2024-08-24 23:21:47 -0300

    feat(): add first component and add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "courses", pathMatch: "full" },
+  {
+    path: "",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+  },
+];
