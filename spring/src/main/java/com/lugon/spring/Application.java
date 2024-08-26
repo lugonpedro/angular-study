d6471fedc760cb2ade1b6fd591f1d1a84ba26254
Pedro Lugon <pedromotta20@gmail.com>
2024-08-26 12:46:06 -0300

    feat(java): add bean to create one register

--- spring/src/main/java/com/lugon/spring/Application.java
+++ spring/src/main/java/com/lugon/spring/Application.java
@@ -1,7 +1,11 @@
 package com.lugon.spring;
 
+import com.lugon.spring.models.Course;
+import com.lugon.spring.repositories.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Fullstack");
+
+			courseRepository.save(c);
+		};
+	}
 }
