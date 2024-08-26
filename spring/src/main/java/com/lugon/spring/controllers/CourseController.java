5c52b3cfa67a361616201c9322828898ac7c7541
Pedro Lugon <pedromotta20@gmail.com>
2024-08-26 12:45:55 -0300

    feat(course): add course read all method

---
+++ spring/src/main/java/com/lugon/spring/controllers/CourseController.java
@@ -0,0 +1,42 @@
+package com.lugon.spring.controllers;
+
+import com.lugon.spring.models.Course;
+import com.lugon.spring.repositories.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository repository;
+
+    @PostMapping
+    public Object create() {
+        return null;
+    }
+
+    @GetMapping
+    public List<Course> readAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Object readById(@PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Object update(@PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
